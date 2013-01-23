56f0858b00b8e82ad9e91172165c0bbb818a4d51
hbc <bcxxxxxx@gmail.com>
2013-01-23 14:54:25 +0800

    added url_filter

--- utils.php
+++ utils.php
@@ -18,9 +18,38 @@ $twig = new Twig_Environment($loader);
  *
  * A static path builder via twig filter.
  */
-$static_url_filter = new Twig_SimpleFilter('static_url', function ($str) {
+$static_url_filter = new Twig_SimpleFilter('static_url', function($str) {
     return STATIC_PATH . $str;
 });
 $twig->addFilter($static_url_filter);
+
+/**
+ * url_filter
+ *
+ * A url builder.
+ * e.g.,
+ *
+ *      /blog/post/id/1 | url
+ *      =>  SITE_PATH/index.php?s=/blog/post/id/1
+ *
+ *      1 | url(prefix='post/id/', base='blog')
+ *      =>  SITE_PATH/index.php?s=/blog/post/id/1
+ */
+$url_filter = new Twig_SimpleFilter('url', function($str, $prefix=null,
+                                                    $base=null) {
+    if ($prefix === null) {
+        $prefix = '';
+    } else {
+        $prefix = trim($prefix, '/') . '/';
+    }
+    if ($base === null) {
+        $base = '';
+    } else {
+        $base = trim($base, '/') . '/';
+    }
+    $str = trim($str, '/');
+    return SITE_PATH . 'index.php?s=/' . $base . $prefix . $str;
+});
+$twig->addFilter($url_filter);
 
 /* End of file utils.php */
